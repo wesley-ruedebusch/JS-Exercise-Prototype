05a17b926dbd0d5d6803aef2736af42fd8710080
Wesley Ruedebusch <w.ruedebusch@gmail.com>
2019-11-13 17:59:15 -0500

    mvp done

--- index.js
+++ index.js
@@ -39,8 +39,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
 
+Person.prototype.eat = function(someFood) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(someFood);
+  };
+}
+
+Person.prototype.poop = function() {
+    this.stomach = [];
+};
+
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
 }
 
 /*
@@ -57,8 +73,24 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
 
+Car.prototype.fill = function(gallons) {
+  this.tank = this.tank+gallons;
+}
+
+Car.prototype.drive = function(distance) {
+  this.odometer = this.odometer+distance;
+  if (this.tank > distance/this.milesPerGallon) {
+    this.tank = this.tank-(distance/this.milesPerGallon);
+  } else {
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }
 }
 
 /*
@@ -68,18 +100,21 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){return `Playing with ${this.favoriteToy}`}  
 
 /* 
   TASK 4
-
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. can return whatever is inside of your constructor function
+  2. . defines function and refers to "this"
+  3.  this will lead to inheritance.
+  4. call or apply has to explicitly defined with the first argument
 */
 
 
